1efba8e998f528fb18247a4a696f02f6be002867
Blas <montanariblas@hotmail.com>
2024-10-15 16:29:11 -0300

    1st

---
+++ app.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    speed: 2000,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: true
+  });
+  $('.brands-slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+});
+
+gsap.from(".title", { duration: 4, x: -200, ease: "bounce.out" });
+gsap.from("#sequential", { duration: 5, opacity: 0, y: -50 });
+gsap.from(".first-grid-container", { duration: 4, x: -200, ease: "bounce.out" });
+
+
