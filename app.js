20184f5dff50b6057564216390a4646cedc9c87e
Blas <montanariblas@hotmail.com>
2024-11-12 17:46:36 -0300

    test

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
   });
 });
 
-gsap.from(".title", { duration: 4, x: -200, ease: "bounce.out" });
+gsap.from(".title-js", { duration: 4, x: -200, ease: "bounce.out" });
 gsap.from("#sequential", { duration: 5, opacity: 0, y: -50 });
 gsap.from(".first-grid-container", { duration: 4, x: -200, ease: "bounce.out" });
 
